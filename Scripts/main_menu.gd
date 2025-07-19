99c0b6ddde801e4f86ddadf00a540cd8e0ebe816
Aboody-123 <a.alzaza09@gmail.com>
2025-07-19 14:28:01 +0100

    Linked to main scene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,8 +4,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	#change to main scene
-	pass
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_options_pressed() -> void:
