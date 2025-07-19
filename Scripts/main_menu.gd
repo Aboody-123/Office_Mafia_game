8482781101aa60ed091fed630cd37f3bc041297e
MrGrub <mrgrub1000@gmail.com>
2025-07-19 17:33:47 +0400

    i hope this works
    sss

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,9 +4,8 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	#change to main scene
-	pass
 
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_options_pressed() -> void:
 	print("options")
