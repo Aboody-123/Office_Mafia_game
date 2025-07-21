3ddc2ef627d9fd31819532e4e8324bbbb4a15991
MrGrub <mrgrub1000@gmail.com>
2025-07-21 19:11:23 +0400

    a lot better just need to fix a bug

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,19 +1,6 @@
 extends Node2D
 
-@onready var pause_menu = $pause_menu
-var paused = false
+@onready var pause_menu = $Player/pause_menu
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		pauseMenu()
-	
-	
-func pauseMenu():
-	if paused:
-		pause_menu.hide()
-		get_tree().paused = false
-	else:
-		pause_menu.show()
-		get_tree().paused = true
 		
 	
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,9 +1,22 @@
 extends Control
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
 
 
+func pauseMenu():
+	if get_tree().paused == true:
+		$CanvasLayer.hide()
+		get_tree().paused = false
+		
+	if get_tree().paused == false:
+		$CanvasLayer.show()
+		get_tree().paused = true
+		
 func _on_resume_pressed() -> void:
-	pass # Replace with function body.
+	pauseMenu()
+	print("resume")
 	
 	
 func _on_options_pressed() -> void:
