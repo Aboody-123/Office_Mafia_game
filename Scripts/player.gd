0b699404023b8128a6c3be0fb57e24303f797ba1
ParkourPlayer <Izushameer@gmail.com>
2025-08-11 09:59:08 +0400

    Revert "Merge branch 'main' into Map_Making"
    This reverts commit da485336ec6cb7460ffadef73f4e5ec5ac9730e0, reversing
    changes made to c181f279ef786e28408c0980ad5dbf2615a99c3f.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
-var speed = 300.0
-const JUMP_VELOCITY = -400.0
-var sprint = 350
+const SPEED = 200
+const JUMP_VELOCITY = -300.0
+
 # the states {0:idle, 1:walking/running, 2:attacking, 3:picking up
-enum States { IDLE, WALKING, SPRINTING, JUMPING,}
-var state = States.IDLE
-var direction 
-
-
+var state = 0
+# where is it facing
+var facingRight = true
 
 
 
@@ -16,9 +14,38 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	handle_state_transitions()
-	perform_state_actions(delta)
+
+	# Handle jump.
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# left and right
+	var direction := Input.get_axis("left", "right")
+	
+	if direction:
+		if (direction == 1):
+			facingRight = true
+		else:
+			facingRight = false
+		velocity.x = direction * SPEED
+		state = 1
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# handles walking
+		state = 0
+	
+	# flips the animation when neccessary
+	if (facingRight):
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
+	
+	# Plays animations when needed
+	if (state == 1):
+		$AnimatedSprite2D.play("walk")
+	elif (state == 0):
+		$AnimatedSprite2D.play("idle")
+	
 	move_and_slide()
 
 
@@ -29,46 +56,3 @@ func _on_health_component_hurt() -> void:
 
 func _on_health_component_dead() -> void:
 	print("dead")
-	
-func handle_state_transitions():
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		state = States.JUMPING
-		#only applies jump strength once
-		velocity.y = JUMP_VELOCITY
-	direction = Input.get_axis("left", "right")
-	if direction != 0:
-		
-		state = States.WALKING
-		
-	elif is_on_floor() and state != States.JUMPING:
-		state = States.IDLE
-		
-	if Input.is_action_pressed("sprint"):
-		state = States.SPRINTING
-	
-func perform_state_actions(delta):
-	match state:
-		States.IDLE:
-			$AnimatedSprite2D.play("idle")
-			velocity.x = move_toward(velocity.x, 0, speed)
-		States.WALKING:
-			$AnimatedSprite2D.play("walk")
-			
-			if direction > 0:
-				$AnimatedSprite2D.flip_h = false
-			elif direction < 0:
-				$AnimatedSprite2D.flip_h = true
-			velocity.x = speed * direction
-		
-		States.JUMPING:
-			pass
-			#add animation
-		States.SPRINTING:
-			velocity.x = sprint * direction
-			
-			if direction > 0:
-				$AnimatedSprite2D.flip_h = false
-			elif direction < 0:
-				$AnimatedSprite2D.flip_h = true
-			
