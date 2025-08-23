a6bf59914867bb3ff322c6373e614beeba163577
MrGrub <mrgrub1000@gmail.com>
2025-08-23 17:59:17 +0400

    fixed pause menu
    pause menu broke for some reason.

---
+++ pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var pause_menu = $".."
+
+func _process(delta: float) -> void:
+	#when escape is pressed then runs func pauseMenu
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+
+
+func pauseMenu():
+	#if its paused already, then hide the menu
+	if get_tree().paused == true:
+		pause_menu.hide()
+		get_tree().paused = false
+		
+	#if its not paused then open the menu
+	elif get_tree().paused == false:
+		pause_menu.show()
+		get_tree().paused = true
+		
+func _on_resume_pressed() -> void:
+	#when paused and resume is hit then unpauses
+	pauseMenu()
+	print("resume")
+	
+	
+func _on_options_pressed() -> void:
+	print("options")
+	#fill in later
+	
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	print("quit")
